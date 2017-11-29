e74812aa989604725989d2af8f6411d8d23ba301
Ahmadi-Aryan <33659473+Ahmadi-Aryan@users.noreply.github.com>
2017-11-29 08:14:09 +0200

    Create Exercitii_foaie.cpp

---
+++ Exercitii_foaie.cpp
@@ -0,0 +1,55 @@
+//Ex 8
+
+/*
+
+int v[128];
+int v2[128];
+int n;
+int nr=0;
+
+void citire()
+{
+    cin>>n;
+}
+
+void to_bin(){
+
+
+
+    while(n!=0){
+
+        v[nr]=n%2;
+        nr++;
+        n/=2;
+
+    }
+}
+
+void change_vector(){
+
+    for(int i = 0; i< nr; i++)
+    {
+        v2[nr-1-i]=v[i];
+    }
+
+}
+
+void afisare()
+{
+    for (int i = 0; i<nr; i++)
+    {
+        cout<<v2[i]<<" ";
+    }int nr=0;
+    cout<<endl;
+}
+
+int main(){
+
+    citire();
+    to_bin();
+    change_vector();
+    afisare();
+
+}
+
+*/
